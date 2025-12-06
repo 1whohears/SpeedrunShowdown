a71a4a6658625db1efa0c1391d500f199a46f68e
1whohears <1whohears28@gmail.com>
2025-12-06 13:29:32 -0600

    tell everyone if the checknether command was successful.

--- src/main/java/com/github/speedrunshowdown/commands/VerifyNetherStructuresCommand.java
+++ src/main/java/com/github/speedrunshowdown/commands/VerifyNetherStructuresCommand.java
@@ -8,14 +8,14 @@ import org.jetbrains.annotations.NotNull;
 
 public class VerifyNetherStructuresCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
                              @NotNull String s, @NotNull String @NotNull [] strings) {
         if (StartCommand.verifyNetherStructures()) {
-            commandSender.sendMessage(ChatColor.GREEN+"The Nether has at least 1 Fortress and at least 1 Bastian " +
-                    "inside the world border!");
+            sender.getServer().broadcastMessage(ChatColor.GREEN
+                    +"The Nether has at least 1 Fortress and at least 1 Bastian inside the world border!");
         } else {
-            commandSender.sendMessage(ChatColor.LIGHT_PURPLE+"The Nether is missing a required Structure! " +
-                    "Please use a different seed!");
+            sender.getServer().broadcastMessage(ChatColor.LIGHT_PURPLE
+                    +"The Nether is missing a required Structure! Please use a different seed!");
         }
         return false;
     }
