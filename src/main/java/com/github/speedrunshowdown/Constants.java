6d6e9e876a4564ce28e39687c61f9f54dae39b91
1whohears <1whohears28@gmail.com>
2025-12-16 15:49:31 -0600

    update to 1.21.11

--- src/main/java/com/github/speedrunshowdown/Constants.java
+++ src/main/java/com/github/speedrunshowdown/Constants.java
@@ -1033,7 +1033,14 @@ public class Constants {
             Material.COPPER_AXE,
             Material.COPPER_SHOVEL,
             Material.COPPER_HOE,
-            Material.COPPER_SWORD
+            Material.COPPER_SWORD,
+            Material.WOODEN_SPEAR,
+            Material.STONE_SPEAR,
+            Material.COPPER_SPEAR,
+            Material.IRON_SPEAR,
+            Material.GOLDEN_SPEAR,
+            Material.DIAMOND_SPEAR,
+            Material.NETHERITE_SPEAR
 	};
 
     public static final Material[] EFFICIENT_TOOLS = {
